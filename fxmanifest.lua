ad9901d66523de8a0a61e19f004d37275287f983
jay-fivem <80186604+jay-fivem@users.noreply.github.com>
2021-11-15 23:17:24 +1030

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,29 +4,10 @@ game 'gta5'
 description 'QB-BusJob'
 version '1.0.0'
 
-ui_page 'html/ui.html'
+shared_script 'config.lua'
 
-client_scripts {
-	"client/main.lua",
-	"client/ui.lua"
-}
+client_script 'client/main.lua'
 
-server_scripts {
-	"main.lua",
-}
+server_script 'server/main.lua'
 
-files {
-    'html/css/bootstrap.min.css',
-    'html/css/bootstrap-switch.css',
-    'html/css/main.css',
-	'html/css/style.css',
-    'html/css/reset.css',	
-	'html/ui.html',
-    'html/ontvang.js',  
-	'html/res/futurastd-medium.css',
-    'html/res/futurastd-medium.eot',
-    'html/res/futurastd-medium.woff',
-    'html/res/futurastd-medium.ttf',
-    'html/res/futurastd-medium.svg',
-	'html/img/BUS_UI.png',
-}
+lua54 'yes'
