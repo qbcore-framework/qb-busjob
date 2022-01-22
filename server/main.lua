66aa5e55402c8788302054fa00f1f77e7f35e4bf
Jay <80186604+jay-fivem@users.noreply.github.com>
2022-01-23 01:01:39 +1030

    fix exploit

--- server/main.lua
+++ server/main.lua
@@ -1,10 +1,18 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterNetEvent('qb-busjob:server:NpcPay', function(Payment)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local randomAmount = math.random(1, 5)
-    local r1, r2 = math.random(1, 5), math.random(1, 5)
-    if randomAmount == r1 or randomAmount == r2 then Payment = Payment + math.random(10, 20) end
-    Player.Functions.AddMoney('cash', Payment)
+    TriggerEvent("qb-busjob:server:NpcPayed", source, Payment)
+end)
+
+RegisterNetEvent('qb-busjob:server:NpcPayed', function(user,Payment)
+    local src = user
+    if source == "" then
+        local Player = QBCore.Functions.GetPlayer(src)
+        local randomAmount = math.random(1, 5)
+        local r1, r2 = math.random(1, 5), math.random(1, 5)
+        if randomAmount == r1 or randomAmount == r2 then Payment = Payment + math.random(10, 20) end
+        Player.Functions.AddMoney('cash', Payment)
+    else
+        DropPlayer(source, "Attempted exploit abuse")
+    end
 end)
