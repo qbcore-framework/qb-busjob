2e5e023e20d06abbe96219f6181393f944a0020f
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2022-08-17 16:27:36 +0100

    Update fr.lua
    small improve

--- locales/fr.lua
+++ locales/fr.lua
@@ -6,19 +6,19 @@ local Translations = {
         drop_off_passengers = 'Déposez les passagers avant d\'arrêter le travail'
     },
     success = {
-        dropped_off = 'Une personne a été déposée',
+        dropped_off = 'Une personne a été déposée à l\'arrêt',
     },
     info = {
         bus = 'Bus Standard',
-        goto_busstop = 'Allez a l\'arrêt de bus',
+        goto_busstop = 'Allez a l\'arrêt de bus suivant',
         busstop_text = '[E] Arrêt de bus',
         bus_plate = 'BUS', -- Can be 3 or 4 characters long (uses random 4 digits)
         bus_depot = 'Dépôt de Bus',
-        bus_stop_work = '[E] Arrêt du travail',
+        bus_stop_work = '[E] Finir sa tournée',
         bus_job_vehicles = '[E] Véhicules de service'
     },
     menu = {
-        bus_header = 'Garage des bus',
+        bus_header = 'Garage de bus',
         bus_close = '⬅ Fermer Menu'
     }
 }
@@ -26,4 +26,4 @@ local Translations = {
 Lang = Locale:new({
     phrases = Translations,
     warnOnMissing = true
-})
+}) 
