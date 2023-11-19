f35890a97b8703c6466fbee687d2e699741c38bc
Vitorh295 <58405514+vitorh295@users.noreply.github.com>
2023-11-19 20:02:30 +0000

    Add files via upload

--- server/main.lua
+++ server/main.lua
@@ -15,12 +15,21 @@ RegisterNetEvent('qb-busjob:server:NpcPay', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local Payment = math.random(15, 25)
-    if Player.PlayerData.job.name == "bus" then
+    if Player.PlayerData.job.name == Config.Jobname then
         if NearBus(src) then
             local randomAmount = math.random(1, 5)
             local r1, r2 = math.random(1, 5), math.random(1, 5)
             if randomAmount == r1 or randomAmount == r2 then Payment = Payment + math.random(10, 20) end
-            Player.Functions.AddMoney('cash', Payment)
+            if Config.Management then
+                exports['qb-management']:AddMoney('bus', Payment)
+            else
+                Player.Functions.AddMoney('cash', Payment)
+            end
+            local chance = math.random(1, 1000)
+            if chance < 10 then
+                Player.Functions.AddItem("cryptostick", 1, false)
+                TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["cryptostick"], "add")
+            end
         else
             DropPlayer(src, Lang:t('error.exploit'))
         end
